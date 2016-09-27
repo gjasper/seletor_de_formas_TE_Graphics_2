2a269dff88e74b7654d9db3677191ebe0f4a8490
gjasper <gustavohj6@gmail.com>
2016-09-27 20:07:47 -0300

    Adicionadas 3 novas formas e criação de log de tempo

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 using namespace std;
 
+#include <stdio.h>
 #include <windows.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
@@ -19,61 +20,57 @@ using namespace std;
 #define WIDTH 250
 #define PI 3.1415926535
 
-int escolha = 3;
+int escolha = 10;
 
 float angle, raio_x, raio_y;
 int i, circle_points = 500;
+
+long unsigned int startDrawningTime, deltaTime;
 
 //Dois Pontos
 void display() {
 
 	glClear (GL_COLOR_BUFFER_BIT); /* Limpa todos os Pixels da Tela */
 
+	startDrawningTime = glutGet(GLUT_ELAPSED_TIME);
+
     switch (escolha)
     {
     case 1:
-
 		glBegin(GL_LINES);
+		glColor3f(255,255,255);
 			glVertex2f (1 + 10, 240);
 			glVertex2f (1 + 10, 1);
 		glEnd();
-
         break;
     case 2:
-
 		glBegin(GL_LINES);
+		glColor3f(255,255,255);
 			glVertex2f (240, 1 + 10);
 			glVertex2f (1, 1 +  10);
 		glEnd();
-
         break;
     case 3:
-
 			/* Marca o inicio do desenho das primitivas, neste exemplo, uma linha */
 		glBegin(GL_TRIANGLES);
-		  //glBegin(GL_LINES);
+		glColor3f(255,255,255);
 			//Triangulo Abaixo
 			glVertex2f (1, 10);
 			glVertex2f (120, 10);
 			glVertex2f (60, 60);
-
 		glEnd();
-
         break;
 	case 4:
-
 		glBegin(GL_QUADS);
+		glColor3f(255,255,255);
 			//Retângulo Abaixo
 			glVertex2f (10, 10);
 			glVertex2f (10, 100);
 			glVertex2f (40, 100);
 			glVertex2f (40, 10);
-
         glEnd();
-
         break;
 	case 5:
-
 		 glBegin(GL_QUADS);
 			//Retângulo Abaixo
 			glVertex2f (10, 10);
@@ -81,33 +78,110 @@ void display() {
 			glVertex2f (100, 100);
 			glVertex2f (100, 10);
         glEnd();
-
-
         break;
 	case 6:
-
-
 		 // Desenha um círculo (elipse) preenchido com a cor corrente
-		 //glBegin(GL_LINE_LOOP);
 		 glBegin(GL_POLYGON);
+		 glColor3f(255,255,255);
 				   raio_x = 80.0;
 				   raio_y = 100.0;
 				   for(i = 0; i < circle_points; i++) {
-					 angle = (2*PI*i)/circle_points;
+					 angle = (7*PI*i)/circle_points;
 					 glVertex2f(125.0+raio_x*cos(angle),125.0+raio_y*sin(angle));
 					 }
 		 glEnd();
-
         break;
     case 7:
+		 // Desenha um cilindro
+		 glBegin(GL_POLYGON_BIT);
+		 glColor3f(255,255,255);
+				   raio_x = 50.0;
+				   raio_y = 25.0;
+				   for(i = 0; i < circle_points; i++) {
+					 angle = (12*PI*i)/circle_points;
+					 glVertex2f(100.0+raio_x*cos(angle),175.0+raio_y*sin(angle));
+					 }
+		 glEnd();
+		 glBegin(GL_POLYGON_BIT);
+		 glColor3f(255,255,255);
+				   raio_x = 50.0;
+				   raio_y = 25.0;
+				   for(i = 0; i < circle_points; i++) {
+					 angle = (12*PI*i)/circle_points;
+					 glVertex2f(100.0+raio_x*cos(angle),75.0+raio_y*sin(angle));
+					 }
+		 glEnd();
+		 glBegin(GL_QUADS);
+		 glColor3f(255,255,255);
+		 glVertex2f(50,75);
+		 glVertex2f(51.5,75);
+		 glVertex2f(51.5,175);
+		 glVertex2f(50,175);
+		 glEnd();
+		 glBegin(GL_QUADS);
+		 glColor3f(255,255,255);
+		 glVertex2f(149,75);
+		 glVertex2f(150.5,75);
+		 glVertex2f(150.5,175);
+		 glVertex2f(149,175);
+		 glEnd();
+        break;
+    case 8:
+		 glBegin(GL_LINE_LOOP);
+		 glColor3f(255,255,255);
+            //Poligono
+			glVertex2f (10, 30);
+			glVertex2f (10, 70);
+			glVertex2f (40, 90);
+			glVertex2f (70, 70);
+            glVertex2f (70, 30);
+            glVertex2f (40, 10);
+            glVertex2f (10, 30);
+         glEnd();
+        break;
+	case 9:
+	    //Bandeira do Brasil
+         glBegin(GL_QUADS);
+            glColor3f(0,255,0);
+            glVertex2f(10,20);
+            glVertex2f(10,180);
+            glVertex2f(240,180);
+            glVertex2f(240,20);
+         glEnd();
+         glBegin(GL_QUADS);
+            glColor3f(255,255,0);
+            glVertex2f(30,100);
+            glVertex2f(125,160);
+            glVertex2f(210,100);
+            glVertex2f(125,40);
+          glEnd();
+        glBegin(GL_POLYGON);
+            glColor3f(0,0,255);
+            raio_x = 45.0;
+            raio_y = 45.0;
+               for(i = 0; i < circle_points; i++) {
+                 angle = (2*PI*i)/circle_points;
+                 glVertex2f(122.5+raio_x*cos(angle),100.0+raio_y*sin(angle));
+                 }
+            glEnd();
+        break;
+    case 10:
         break;
     default:
         break;
     }
 
+	FILE * fp;
+	fp = fopen ("timelog.txt", "a+");
+
+	deltaTime = glutGet(GLUT_ELAPSED_TIME) - startDrawningTime;
+	fprintf(fp, "%d %d \n", deltaTime, escolha);
+
+   fclose(fp);
+
+    //cout << deltaTime;
     glFlush (); /*Libera o Buffer Principal */
 }
-
 
 void menuFx()
 {
@@ -117,8 +191,12 @@ void menuFx()
         cout << "[F3] displayTriangulo.\n";
         cout << "[F4] displayRetangulo.\n";
 		cout << "[F5] displayQuadrado.\n";
-        cout << "[F6] displayCirculoo.\n";
-        cout << "[F7] Finalizar o programa.\n\n";
+        cout << "[F6] displayCirculo.\n";
+        cout << "[F7] displayCilindro.\n";
+        cout << "[F8] displayPoligono.\n";
+        cout << "[F9] displayBandeira.\n";
+        cout << "[F10] Finalizar o programa.\n\n";
+
 
 }
 
@@ -147,6 +225,15 @@ void menu(int op){
             escolha = 6;
             break;
         case 7:
+            escolha = 7;
+            break;
+        case 8:
+            escolha = 8;
+            break;
+        case 9:
+            escolha = 9;
+            break;
+        case 10:
             glutDestroyWindow(window);
             exit(0);
             break;
@@ -166,6 +253,9 @@ void createMenu(){
     glutAddMenuEntry("Opcao 4", 4);
 	glutAddMenuEntry("Opcao 5", 5);
     glutAddMenuEntry("Opcao 6", 6);
+    glutAddMenuEntry("Opcao 7", 7);
+    glutAddMenuEntry("Opcao 8", 8);
+    glutAddMenuEntry("Opcao 9", 9);
     glutAddMenuEntry("Sair", 0);
 
     myMenu = glutCreateMenu(menu);
@@ -174,7 +264,6 @@ void createMenu(){
     glutAttachMenu(GLUT_RIGHT_BUTTON);
 
 }
-
 
 void specialKey(GLint key, GLint x, GLint y){
 
@@ -200,15 +289,22 @@ void specialKey(GLint key, GLint x, GLint y){
                 escolha = 6;
             break;
         case GLUT_KEY_F7:
+                escolha = 7;
+            break;
+        case GLUT_KEY_F8:
+                escolha = 8;
+            break;
+        case GLUT_KEY_F9:
+                escolha = 9;
+            break;
+        case GLUT_KEY_F10:
                 exit(1);
             break;
         default:
-            system("cls");
             break;
 
     }
 }
-
 
 void init(){
     glClearColor (0.0, 0.0, 0.0, 0.0);  /* Define qual ser a cor do Fundo ao Chamar o GLCLear */
@@ -216,7 +312,6 @@ void init(){
     glLoadIdentity();
     gluOrtho2D(0.0, WIDTH, 0.0, HEIGHT);
 }
-
 
 int main(int argc, char** argv) {
     int mode = GLUT_SINGLE | GLUT_RGB;
